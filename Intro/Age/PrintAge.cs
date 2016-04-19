747b8a21b62aa016b57b9534ac4f9587829caa8a
velev1 <velev_bg@mail.bg>
2016-04-20 01:33:46 +0300

    Intro programming

---
+++ Intro/Age/PrintAge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class PrintAge
+{
+    static void Main()
+    {
+        DateTime today = DateTime.Now;
+        string dateFormat = "MM'.'dd'.'yyyy";
+        string input = (Console.ReadLine());
+        DateTime birthDate = DateTime.ParseExact(input, dateFormat, null); 
+        ushort age = (ushort)((today - birthDate).TotalDays / 365.25);
+        Console.WriteLine(age);
+        Console.WriteLine(age + 10);
+    }
+}
+
---
+++ Intro/Sqrt/SquareRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        double result = Math.Sqrt(12345);
+        Console.WriteLine(result);
+    }
+}
+
