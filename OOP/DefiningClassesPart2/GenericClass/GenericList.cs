edf22451c7ff4d1d22630781d049fd3c628b68ac
velev1 <velev_bg@mail.bg>
2016-12-31 00:46:55 +0200

    Implement class GenericList with some methods which are described in the class.

---
+++ OOP/DefiningClassesPart2/GenericClass/GenericList.cs
@@ -0,0 +1,155 @@
+﻿namespace GenericClass
+{
+    using System;
+    using System.Text;
+
+    public class GenericList<T>
+    {
+        private T[] collection;
+        private const int initialCapacity = 4;
+
+        public GenericList(int capacity = initialCapacity)
+        {
+            collection = new T[capacity];
+            Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return collection.Length;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return collection[index];
+            }
+            set
+            {
+                collection[index] = value;
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (Count == collection.Length)
+            {
+                collection = IncCollectionCapacity();
+                collection[Count] = value;
+                Count++;
+            }
+            else
+            {
+                collection[Count] = value;
+                Count++;
+            }
+        }
+        /// <summary>
+        /// removing element by index
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                if (i < Count - 1)
+                {
+                    collection[i] = collection[i + 1];
+                }
+                else
+                {
+                    collection[i] = default(T);
+                }
+            }
+            Count--;
+        }
+        /// <summary>
+        /// inserting element at given position
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        public void InsertAt(int index, T value)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new IndexOutOfRangeException("The index must be possitive number and must be less or equal to the Count of the collection.");
+            }
+            if (Count == collection.Length)
+            {
+                collection = IncCollectionCapacity();
+            }
+            Count++;
+            for (int i = Count - 1; i > index - 1; i--)
+            {
+                if (i == 0)
+                {
+                    break;
+                }
+                collection[i] = collection[i - 1];
+            }
+            collection[index] = value;
+
+        }
+        /// <summary>
+        /// Increasing current capacity of the collection by multyply it with 2
+        /// </summary>
+        private T[] IncCollectionCapacity()
+        {
+            var incresedCollection = new T[2 * collection.Length];
+
+            for (int i = 0; i < Count; i++)
+            {
+                incresedCollection[i] = collection[i];
+            }
+            collection = incresedCollection;
+            return collection;
+        }
+        /// <summary>
+        /// Clear the collection and set the capacity to Initial capacity (4)
+        /// </summary>
+        public void Clear()
+        {
+            var clearedCol = new T[initialCapacity];
+            collection = clearedCol;
+            Count = 0;
+        }
+        /// <summary>
+        /// finding index by its value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int indexOf(T value, int index = 0)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                if (collection[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+
+            for (int i = 0; i < Count; i++)
+            {
+                output.Append(collection[i].ToString());
+                if (i < Count - 1)
+                {
+                    output.Append(", ");
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ OOP/DefiningClassesPart2/GenericClass/Start.cs
@@ -0,0 +1,68 @@
+﻿//Write a generic class GenericList<T> that keeps a list of elements of some parametric type T.
+//Keep the elements of the list in an array with fixed capacity which is given as parameter in the class constructor.
+//Implement methods for adding element, 
+//accessing element by index, 
+//removing element by index, inserting element at given position, 
+//clearing the list, finding element by its value and ToString().
+//Check all input parameters to avoid accessing elements at invalid positions.
+
+
+namespace GenericClass
+{
+    using System;
+    public class Start
+    {
+        static void Main()
+        {
+            //Empty constructor with the default capacity. If some value is written like parameter in the constructor
+            //the capacity will be equal to this parameter.
+            GenericList<int> col = new GenericList<int>();
+            //add some elements to the collection
+            col.Add(2);
+            col.Add(77);
+            col.Add(33);
+            col.Add(7447);
+            Console.WriteLine("the elements of the collection: " + col.ToString());
+            Console.WriteLine("The capacity = " + col.Capacity);
+            Console.WriteLine("The element at index[1] is: " + col[1]);
+            //set new value at index [1] 
+            col[1] = 666;
+            Console.WriteLine("The new value in index [1] is: " + col[1]);
+
+            Console.WriteLine("Count of the collection = " + col.Count);
+            //Removing element at index [0]
+            col.RemoveAt(0);
+            Console.WriteLine();
+            Console.WriteLine("Collection after removing: " + col.ToString());
+            Console.WriteLine("Count of the collection after removing: " + col.Count);
+            //inserting element in the collection
+            Console.WriteLine("collection before inserting: " + col.ToString());
+            col.InsertAt(2, 9999999);
+            Console.WriteLine("collection after inserting: " + col.ToString());
+
+            //Clear and print capacity and Count
+            col.Clear();
+            Console.WriteLine();
+            Console.WriteLine("After the clear -----------------------");
+            Console.WriteLine("Capacity = " + col.Capacity);
+            Console.WriteLine("Count = " + col.Count);
+            Console.WriteLine("Collection: " + col.ToString());
+
+            //finding index by its value if exist. If do not exist return -1
+            col.Add(2);
+            col.Add(3);
+
+            int value = 3;
+            int index = col.indexOf(value);
+            if (index > 0)
+            {
+                Console.WriteLine("The element with value: {0} is on index: {1}", value, index);
+            }
+            else
+            {
+                Console.WriteLine("The element with value: {0} is not found.",value);
+            }
+            Console.WriteLine(col.ToString());
+        }
+    }
+}
