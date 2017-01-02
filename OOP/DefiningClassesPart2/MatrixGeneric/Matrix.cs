9972b8752959f94056a503e59c55b07f2a6c297e
velev1 <velev_bg@mail.bg>
2017-01-03 01:34:54 +0200

    matrix class was added.

---
+++ OOP/DefiningClassesPart2/MatrixGeneric/Matrix.cs
@@ -0,0 +1,188 @@
+﻿namespace MatrixGeneric
+{
+
+    using System;
+    public class Matrix<T> where T : IComparable<T>
+    {
+        public int Rows { get; private set; }
+
+        public int Cols { get; private set; }
+
+        T[,] ma3x;
+
+        public T this[int row, int col]
+        {
+            get
+            {
+                if ((row > Rows - 1) || (col > Cols - 1) || (row < 0) || (col < 0))
+                {
+                    throw new IndexOutOfRangeException("The index is outside of the bounds of the matrix.");
+                }
+                return ma3x[row, col];
+            }
+            set
+            {
+                if ((row > Rows - 1) || (col > Cols - 1) || (row < 0) || (col < 0))
+                {
+                    throw new IndexOutOfRangeException("The index is outside of the bounds of the matrix.");
+                }
+                ma3x[row, col] = value;
+            }
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            ma3x = new T[Rows, Cols];
+        }
+
+        /// <summary>
+        /// addition of two matrices 
+        /// </summary>
+        /// <param name="m1"></param>
+        /// <param name="m2"></param>
+        /// <returns></returns>
+        public static Matrix<T> operator +(Matrix<T> m1, Matrix<T> m2)
+        {
+            if ((m1.Rows != m2.Rows) || (m1.Cols != m2.Cols))
+            {
+                throw new ArgumentException("The dimentions of the two matrices must be the same");
+            }
+            else
+            {
+                var newMatrix = new Matrix<T>(m1.Rows, m1.Cols);
+                for (int row = 0; row < m1.Rows; row++)
+                {
+                    for (int col = 0; col < m1.Cols; col++)
+                    {
+                        newMatrix[row, col] = (dynamic)m1[row, col] + m2[row, col];
+                    }
+                }
+                return newMatrix;
+            }
+        }
+
+        /// <summary>
+        /// Substraction of two matrices
+        /// </summary>
+        /// <param name="m1"></param>
+        /// <param name="m2"></param>
+        /// <returns></returns>
+        public static Matrix<T> operator -(Matrix<T> m1, Matrix<T> m2)
+        {
+            if ((m1.Rows != m2.Rows) || (m1.Cols != m2.Cols))
+            {
+                throw new ArgumentException("The dimentions of the two matrices must be the same");
+            }
+            else
+            {
+                var newMatrix = new Matrix<T>(m1.Rows, m1.Cols);
+                for (int row = 0; row < m1.Rows; row++)
+                {
+                    for (int col = 0; col < m1.Cols; col++)
+                    {
+                        newMatrix[row, col] = (dynamic)m1[row, col] - m2[row, col];
+                    }
+                }
+                return newMatrix;
+            }
+        }
+
+        /// <summary>
+        /// Multiplication between matrix and scalar
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static Matrix<T> operator *(Matrix<T> matrix, T number)
+        {
+            var newMatrix = new Matrix<T>(matrix.Rows, matrix.Cols);
+
+            for (int row = 0; row < matrix.Rows; row++)
+            {
+                for (int col = 0; col < matrix.Cols; col++)
+                {
+                    newMatrix[row, col] = matrix[row, col] * (dynamic)number;
+                }
+            }
+            return newMatrix;
+        }
+
+        /// <summary>
+        /// matrices  multiplication
+        /// </summary>
+        /// <param name="m1"></param>
+        /// <param name="m2"></param>
+        /// <returns></returns>
+        public static Matrix<T> operator *(Matrix<T> m1, Matrix<T> m2)
+        {
+            if (m1.Cols != m2.Rows)
+            {
+                throw new ArgumentException("The columns of the first matrix must be equal to the rows of the second."); 
+            }
+            else
+            {
+                var newMatrix = new Matrix<T>(m1.Rows, m2.Cols);
+                T temp = default(T);
+
+                for (int row = 0; row < newMatrix.Rows; row++)
+                {
+                    for (int col = 0; col < newMatrix.Cols; col++)
+                    {
+                        for (int i = 0; i < newMatrix.Rows; i++)
+                        {
+
+                            temp += (dynamic)m1[row, i] * m2[i, col];
+                        }
+                        newMatrix[row, col] = temp;
+                        temp = default(T);
+                    }
+                }
+                return newMatrix;
+            }
+        }
+
+        /// <summary>
+        /// check if there are zeros in the matrix
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>true if there are NO zeros and false if there are zero/s</returns>
+        public static bool operator true(Matrix<T> matrix)
+        {
+            for (int row = 0; row < matrix.Rows; row++)
+            {
+                for (int col = 0; col < matrix.Cols; col++)
+                {
+                    if (matrix[row,col].Equals(0))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// check if there are zeros in the matrix
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>true if there are NO zeros and false if there are zero/s</returns>
+        public static bool operator false(Matrix<T> matrix)
+        {
+
+            for (int row = 0; row < matrix.Rows; row++)
+            {
+                for (int col = 0; col < matrix.Cols; col++)
+                {
+                    if (matrix[row, col].Equals(0))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ OOP/DefiningClassesPart2/MatrixGeneric/Start.cs
@@ -0,0 +1,98 @@
+﻿//Define a class Matrix<T> to hold a matrix of numbers (e.g. integers, floats, decimals).
+//Implement an indexer this[row, col] to access the inner matrix cells.
+
+//Implement the operators + and - (addition and subtraction of matrices of the same size) and* for matrix multiplication.
+//Throw an exception when the operation cannot be performed.
+//Implement the true operator (check for non-zero elements).
+
+namespace MatrixGeneric
+{
+    using System;
+    public class Start
+    {
+        public static void Main()
+        {
+            var m1 = new Matrix<int>(2, 2);
+            var m2 = new Matrix<int>(2, 1);
+            //test
+            m1[0, 0] = 2;
+            m1[0, 1] = 1;
+            m1[1, 0] = 1;
+            m1[1, 1] = 1;
+
+            m2[0, 0] = 3;
+            m2[1, 0] = 3;
+
+
+            if (m1)
+            {
+                Console.WriteLine("there are no zeros in this matrix.");
+            }
+            else
+            {
+                Console.WriteLine("zero detected");
+            }
+
+            //for (int row = 0; row < m1.Rows; row++)
+            //{
+            //    for (int col = 0; col < m1.Cols; col++)
+            //    {
+            //        m1[row, col] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            //for (int row = 0; row < m2.Rows; row++)
+            //{
+            //    for (int col = 0; col < m2.Cols; col++)
+            //    {
+            //        m2[row, col] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+
+            Console.WriteLine("first matrix-------");
+            for (int i = 0; i < m1.Rows; i++)
+            {
+                for (int j = 0; j < m1.Cols; j++)
+                {
+                    Console.Write(" " + m1[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("second matrix--------");
+            for (int i = 0; i < m2.Rows; i++)
+            {
+                for (int j = 0; j < m2.Cols; j++)
+                {
+                    Console.Write(" " + m2[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            var res = m1 * m2;
+            Console.WriteLine("The new matrix after multiply ----");
+            for (int row = 0; row < res.Rows; row++)
+            {
+                for (int col = 0; col < res.Cols; col++)
+                {
+                    Console.Write(" " + res[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("multiply with scalar ---");
+            res = res * 2;
+            for (int row = 0; row < res.Rows; row++)
+            {
+                for (int col = 0; col < res.Cols; col++)
+                {
+                    Console.Write(" " + res[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
