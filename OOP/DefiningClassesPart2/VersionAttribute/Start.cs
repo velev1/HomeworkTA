67c9d567fe5a174fff57accb96c417b724572565
velev1 <velev_bg@mail.bg>
2017-01-03 16:59:03 +0200

    VersionAttribute added.

---
+++ OOP/DefiningClassesPart2/VersionAttribute/Start.cs
@@ -0,0 +1,23 @@
+﻿namespace VersionAttribute
+{
+    using System;
+
+    [Version(1, 0)]
+    [Version(1, 1)]
+    [Version(1, 2)]
+    public class Start
+    {
+        static void Main()
+        {
+            Type type = typeof(Start);
+            object[] allAttributes = type.GetCustomAttributes(false);
+            foreach (VersionAttribute versionAttribute in allAttributes)
+            {
+                Console.WriteLine(
+                    "This version is {0} ",
+                    versionAttribute.Full);
+            }
+
+        }
+    }
+}
---
+++ OOP/DefiningClassesPart2/VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace VersionAttribute
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct |
+      AttributeTargets.Class | AttributeTargets.Interface |
+      AttributeTargets.Enum | AttributeTargets.Method,
+      AllowMultiple = true)]
+    public class VersionAttribute : System.Attribute
+    {
+        public VersionAttribute(int major, int minor)
+        {
+            this.Major = major;
+            this.Minor = minor;
+            this.Full = this.Major + "." + this.Minor;
+        }
+
+        public int Major { get; private set; }
+        public int Minor { get; private set; }
+        public string Full { get; private set; }
+    }
+}
